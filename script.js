b88eeff18909509da84554def99933964199d9e3
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2025-01-17 23:00:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let hexCode = "#";
+  for (let i = 0; i < shortHex.length; i++) { 
+    console.log(shortHex[i]);
+    if (shortHex[i] != "#") {
+        hexCode += shortHex[i]+shortHex[i];
+    }
+  }
+	return hexCode;
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+alert(extendHex(shortHex)); 
